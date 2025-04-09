b9fc7d15ec1643275569292eb9e1b43e5457f7f1
julien9969 <julien.roux404@gmail.com>
2025-04-08 20:11:59 -0400

    AE from https://arxiv.org/html/2407.03990v1

--- AE.py
+++ AE.py
@@ -7,43 +7,32 @@ from torchvision import transforms
 from PIL import Image
 import os
 import matplotlib.pyplot as plt
-import logging
+import logging, time, tqdm, sys
 
-logging.basicConfig(level=logging.INFO, format='%(message)s - %(asctime)s - %(levelname)s')
+logging.basicConfig(level=logging.INFO, format='%(levelname)s | %(message)s - %(asctime)s', datefmt='%H:%M:%S')
 
 IMAGE_SIZE = 128
-
-transform = transforms.Compose([
-    transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
-    transforms.ToTensor(),
-    # transforms.Normalize((0.5,), (0.5,))
-])
-
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# NUM_EPOCHS = 500
-# BATCH_SIZE = 64
 LR = 0.001
 
-NUM_EPOCHS = 1000
+NUM_EPOCHS = 250
 BATCH_SIZE = 128
 
 HIDDEN_DIM = 256
 LATENT_DIM = 1024
-# HIDDEN_DIM = 514
-# LATENT_DIM = 256
-WEIGHT_DECAY = 1e-5
 
-# CONV_N_FILTERS_1 = 6 # multiple de 3 idealement?
-# CONV_N_FILTERS_2 = 16 #
 NUM_CHANNELS = 3
-CONV_FILTERS_1 = 16 
-CONV_FILTERS_2 = 32
-CONV_FILTERS_3 = 64
-CONV_FILTERS_4 = 128 
+CONV_FILTERS_1 = 32 
+CONV_FILTERS_2 = 64
+CONV_FILTERS_3 = 128
+CONV_FILTERS_4 = 256 
 
+BOTTLENECK_FILTERS = 64
 
-
-print(f"Using device: {DEVICE}")
+transform = transforms.Compose([
+    transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
+    transforms.ToTensor(),
+])
 
 class CustomDataset(Dataset):
     def __init__(self, root_dir, transform=None):
@@ -62,116 +51,151 @@ class CustomDataset(Dataset):
         return image, 0  # Dummy label (not used in VAE)
 
 
-class Autoencoder(nn.Module):
+class Encoder(nn.Module):
     def __init__(self):
-        super(Autoencoder, self).__init__()
+        super().__init__()
         self.encoder = nn.Sequential(
-            nn.Conv2d(NUM_CHANNELS, CONV_FILTERS_1, kernel_size=3, stride=2, padding=1), 
-            nn.ReLU(True),
-            nn.Conv2d(CONV_FILTERS_1, CONV_FILTERS_2, kernel_size=3, stride=2, padding=1), 
-            nn.ReLU(True),
-            nn.Conv2d(CONV_FILTERS_2, CONV_FILTERS_3, kernel_size=3, stride=2, padding=1), 
-            nn.ReLU(True),
-            nn.Conv2d(CONV_FILTERS_3, CONV_FILTERS_4, kernel_size=3, stride=2, padding=1),
-            nn.ReLU(True),
+            # convolutional layers
+            nn.Conv2d(NUM_CHANNELS, CONV_FILTERS_1, kernel_size=3, padding=1), # stride=2, 
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(CONV_FILTERS_1, CONV_FILTERS_2, kernel_size=3, padding=1), # stride=2, 
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(CONV_FILTERS_2, CONV_FILTERS_3, kernel_size=3, padding=1), # stride=2, 
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(CONV_FILTERS_3, CONV_FILTERS_4, kernel_size=3, padding=1), # stride=2,
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+
+            # bottleneck
+            nn.Conv2d(CONV_FILTERS_4, BOTTLENECK_FILTERS, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
         )
 
+    def forward(self, x):
+        return self.encoder(x)
+
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super().__init__()
         self.decoder = nn.Sequential(
+            # bottleneck
+            nn.ConvTranspose2d(BOTTLENECK_FILTERS, CONV_FILTERS_4, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+
             nn.ConvTranspose2d(CONV_FILTERS_4, CONV_FILTERS_3, kernel_size=3, stride=2, padding=1, output_padding=1), 
-            nn.ReLU(True),
+            nn.ReLU(inplace=True),
             nn.ConvTranspose2d(CONV_FILTERS_3, CONV_FILTERS_2, kernel_size=3, stride=2, padding=1, output_padding=1), 
-            nn.ReLU(True),
+            nn.ReLU(inplace=True),
             nn.ConvTranspose2d(CONV_FILTERS_2, CONV_FILTERS_1, kernel_size=3, stride=2, padding=1, output_padding=1), 
-            nn.ReLU(True),
+            nn.ReLU(inplace=True),
             nn.ConvTranspose2d(CONV_FILTERS_1, NUM_CHANNELS, kernel_size=3, stride=2, padding=1, output_padding=1), 
             nn.Sigmoid()
         )
 
     def forward(self, x):
-        x = self.encoder(x)
-        x = self.decoder(x)
-        return x
+        return self.decoder(x)
+
+class Autoencoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+
+        residual = x - decoded
+        return decoded, residual
+
+
+def autoencoder_loss(x, recon_x, residual):
+    recon_loss = F.mse_loss(recon_x, x)
+    residual_loss = F.mse_loss(residual, torch.zeros_like(residual))
+    return recon_loss + residual_loss
 
 
 def train(train_loader, valid_loader, save_path):
-    # model = VAE(input_size=IMAGE_SIZE).to(DEVICE)
     model = Autoencoder().to(DEVICE)
-    mse_loss = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=LR)
+
+    # 10 epochs sans amélioration on diminue le learning rate
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10)
 
     for epoch in range(NUM_EPOCHS):
         model.train()
         train_loss = 0
-        for data, _ in train_loader:
-            # print(data.shape) # BATCH, COLOR, SIZE, SIZE
-            data = data.to(DEVICE)
-            optimizer.zero_grad()
-            recon_batch = model(data)
-            loss = mse_loss(recon_batch, data)
-            loss.backward()
-            optimizer.step()
-            train_loss += loss.item()
-        
-        avg_loss = train_loss / len(train_loader.dataset)
-        logging.info(f"Epoch {epoch + 1}, Loss: {avg_loss:.6f}")
-        if (epoch + 1) % 50 == 0:
-            evaluate(model, valid_loader, mse_loss)
+        with tqdm.tqdm(train_loader, desc=f"Epoch {epoch+1}/{NUM_EPOCHS}", unit="batch", position=0, leave=False, ncols=100) as t_loader:
+            for data, _ in t_loader:
+                data = data.to(DEVICE)
+                optimizer.zero_grad()
             
+                recon_batch, residual_batch = model(data)
+                loss = autoencoder_loss(data, recon_batch, residual_batch)
+                
+                loss.backward()
+                optimizer.step()
+                train_loss += loss.item()
+        
+        scheduler.step(train_loss)
+        
+        val_loss = 0.0
+        model.eval()
+        with torch.no_grad():
+            for inputs, _ in valid_loader:
+                inputs = inputs.to(DEVICE)
+                decoded, residual = model(inputs)
+                loss = autoencoder_loss(inputs, decoded, residual)
+                val_loss += loss.item()
+
+        val_loss /= len(valid_loader)
+        train_loss /= len(train_loader)
+        scheduler.step(val_loss)
+
+        logging.info(f"Epoch [{epoch+1}/{NUM_EPOCHS}] - Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}")
+        
+        if (epoch + 1) % 20 == 0:
+            show_image(model, valid_loader)
     
+    show_image(model, valid_loader)
     torch.save(model.state_dict(), save_path)
     logging.info(f"Model saved to {save_path}")
 
-def evaluate(model, valid_loader, mse_loss):
+
+def show_image(model, valid_loader):
     model.eval()
-    test_loss = 0
+
     with torch.no_grad():
-        for data, _ in valid_loader:
-            data = data.to(DEVICE)
-            recon_batch = model(data)
-            test_loss += mse_loss(recon_batch, data).item()
-
-        test_loss /= len(valid_loader.dataset)
-        logging.info('Test Loss: {:.6f}'.format(test_loss))
-
-        # On reprend data du debut prcq les batch sont trop petits à la fin
         data = next(iter(valid_loader))[0].to(DEVICE)
-        recon_batch = model(data)
-        num_images = 8
-        fig, axs = plt.subplots(2, 8, figsize=(12, 3))
+        decoded, residual = model(data)
 
-        # print("===========", data.shape)
-        # print("==--------=========", recon_batch.shape)
-        # print("===========++", data[0, :].shape)
+        num_images = min(8, data.size(0))
+        fig, axs = plt.subplots(3, num_images, figsize=(num_images * 2, 6))
+
         for i in range(num_images):
-            axs[0, i].imshow(data[i, :].cpu().squeeze().permute(1, 2, 0))
+            # Original
+            axs[0, i].imshow(data[i].cpu().permute(1, 2, 0))
             axs[0, i].set_title("Original")
             axs[0, i].axis("off")
 
-            axs[1, i].imshow(recon_batch[i, :].cpu().permute(1, 2, 0))
+            # Reconstructed
+            axs[1, i].imshow(decoded[i].cpu().permute(1, 2, 0))
             axs[1, i].set_title("Reconstructed")
             axs[1, i].axis("off")
 
+            # Residual
+            res_img = (residual[i].cpu().permute(1, 2, 0) + 0.5).clamp(0, 1)
+            axs[2, i].imshow(res_img)
+            axs[2, i].set_title("Residual")
+            axs[2, i].axis("off")
+
+    plt.tight_layout()
     plt.show()
 
-# def test_model(model_path, image_path):
-#     model = VAE_Class(256).to(DEVICE)
-#     model.load_state_dict(torch.load(model_path))
-#     model.eval()
-
-#     image = Image.open(image_path).convert('L')
-#     image = transform(image).unsqueeze(0).to(DEVICE)
-    
-#     with torch.no_grad():
-#         mu, logvar = model.encode(image)
-#         latent_vector = model.reparameterize(mu, logvar)
-#         recon_image = model.decode(latent_vector).cpu().squeeze().numpy()
-    
-#     fig, axes = plt.subplots(1, 2, figsize=(8, 4))
-#     axes[0].imshow(image.cpu().numpy().squeeze().permute(1, 2, 0))
-#     axes[0].set_title('Original')
-#     axes[1].imshow(recon_image.permute(1, 2, 0))
-#     axes[1].set_title('Reconstructed')
-#     plt.show()
 
 if __name__ == "__main__":
     dataset_path = './dataset/dogs'
@@ -181,8 +205,24 @@ if __name__ == "__main__":
     train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
     valid_loader = DataLoader(valid_dataset, batch_size=BATCH_SIZE, shuffle=False)
     
-    model_path = "vae_model.pth"
+    model_name = f"AE_{IMAGE_SIZE}_{NUM_EPOCHS}.pth"
+
+    if not os.path.exists("train_models"):
+        os.makedirs("train_models")
+    model_path = os.path.join("train_models", model_name)
+
+    start = time.time()
+    logging.info("Training started")
+    logging.info(f"Model name: {model_name}")
+    logging.info(f"Batch size: {BATCH_SIZE}")
+    logging.info(f"Learning rate: {LR}")
+    logging.info(f"Epochs: {NUM_EPOCHS}")
+    logging.info(f"Image size: {IMAGE_SIZE}")
+    logging.info(f"Device: {DEVICE}")
+        
     train(train_loader, valid_loader, model_path)
+
+    logging.info(f"Training completed in {(time.time() - start)//60:.2f} min")
     
     # test_image_path = './dataset/dogs/valid/n02085936_1390.JPEG'
     # test_model(model_path, test_image_path)
